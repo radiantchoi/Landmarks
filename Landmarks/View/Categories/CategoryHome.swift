649cb319f3238378cc4cfdee63ecb1f4a6c3e1a2
Gordon Choi <thefirate@gmail.com>
2023-06-30 16:28:13 +0900

    Section 1: Add a category view

---
+++ Landmarks/View/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Gordon Choi on 2023/06/30.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
