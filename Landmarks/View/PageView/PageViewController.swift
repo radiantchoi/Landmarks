4383d1a66ebcce4e53e3e0c8e3a13074dd4889ff
Gordon Choi <thefirate@gmail.com>
2023-07-04 02:28:11 +0900

    Section 3: Track the page in a SwiftUI View's state

--- Landmarks/View/PageView/PageViewController.swift
+++ Landmarks/View/PageView/PageViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
+    @Binding var currentPage: Int
     
     func makeCoordinator() -> Coordinator {
         Coordinator(parent: self)
@@ -21,19 +22,20 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             navigationOrientation: .horizontal
         )
         pageViewController.dataSource = context.coordinator
+        pageViewController.delegate = context.coordinator
         
         return pageViewController
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [context.coordinator.controllers[0]],
+            [context.coordinator.controllers[currentPage]],
             direction: .forward,
             animated: true
         )
     }
     
-    final class Coordinator: NSObject, UIPageViewControllerDataSource {
+    final class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
         var controllers = Array<UIViewController>()
         
@@ -66,6 +68,17 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             }
             
             return controllers[index + 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController,
+                                didFinishAnimating finished: Bool,
+                                previousViewControllers: [UIViewController],
+                                transitionCompleted completed: Bool) {
+            if completed,
+               let visibleViewController = pageViewController.viewControllers?.first,
+               let index = controllers.firstIndex(of: visibleViewController) {
+                parent.currentPage = index
+            }
         }
     }
 }
