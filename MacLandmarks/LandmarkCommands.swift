8a46860104be55cf74677c88b0ba1ef6c8a0a0a2
Gordon Choi <thefirate@gmail.com>
2023-07-06 04:44:53 +0900

    Section 5: Add a built-in menu command

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Gordon Choi on 2023/07/06.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
